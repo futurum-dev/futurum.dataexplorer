34566ad608b84b717bb0bf850025ca5d4c8296a9
futurum-dev <me@futurum.dev>
2023-11-22 00:20:03 +0000

    Added "/query-ai" endpoints that connect to OpenAI

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
 # futurum.dataexplorer
 
-## Postgres
+## Setup OpenAPI Key
+Follow these instructions to setup your OpenAPI key:
+https://platform.openai.com/docs/quickstart/step-2-setup-your-api-key
 
+## Spark
+In the 'server' folder, run:
+```
+docker-compose up
+```
+
+## Postgres
 ```
 docker run --name dataexplorer-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=root -e POSTGRES_DB=dataexplorer -p 5432:5432 postgres
+```
+
+## Jupyter-Labs
+```
+docker build \
+  -f cluster-base.Dockerfile \
+  -t cluster-base .
+```
+
+```
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
+  -f jupyterlab.Dockerfile \
+  -t jupyterlab .
 ```
--- server/Futurum.DataExplorer.Spark/main.py
+++ server/Futurum.DataExplorer.Spark/main.py
@@ -11,6 +11,10 @@ from pydantic import BaseModel
 from pyspark.sql.functions import *
 from pyspark.sql.session import SparkSession
 
+import json
+
+from openai import OpenAI
+
 app = FastAPI()
 
 origins = ["*"]
@@ -25,7 +29,7 @@ app.add_middleware(
 
 
 class FilterType(str, Enum):
-    noneFilter = "equalsFilter"
+    equalsFilter = "equalsFilter"
     orFilter = "inFilter"
     notOrFilter = "notInFilter"
 
@@ -38,6 +42,14 @@ class Filter(BaseModel):
 
 class Query(BaseModel):
     drilledColumn: str
+    aggregateColumns: List[str]
+    filters: List[Filter]
+    top: int
+    bottom: int
+
+
+class Query2(BaseModel):
+    drilledColumns: List[str]
     aggregateColumns: List[str]
     filters: List[Filter]
     top: int
@@ -73,11 +85,200 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/query-ai")
+async def data(spark: SparkSession = Depends(get_spark_session)):
+    user_question = "for the United States and Germany, show by sport and country. show the top 5 and the bottom 3"
+
+    json_schema = """
+    {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "definitions": {
+    "FilterType": {
+      "type": "string",
+      "enum": ["equalsFilter", "inFilter", "notInFilter"]
+    },
+    "Filter": {
+      "type": "object",
+      "properties": {
+        "column": {
+          "type": "string"
+        },
+        "values": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "type": {
+          "$ref": "#/definitions/FilterType"
+        }
+      },
+      "required": ["column", "values", "type"]
+    },
+    "Query": {
+      "type": "object",
+      "properties": {
+        "drilledColumns": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "aggregateColumns": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "filters": {
+          "type": "array",
+          "items": {
+            "$ref": "#/definitions/Filter"
+          }
+        },
+        "top": {
+          "type": "integer"
+        },
+        "bottom": {
+          "type": "integer"
+        }
+      },
+      "required": ["drilledColumns", "aggregateColumns", "filters", "top", "bottom"]
+    }
+  }
+}
+
+-----------------
+
+"drilledColumns" is an array of columns that will be used to group the data. For example, if we want to see the data by country and sport, then the array should contain "country" and "sport".
+
+Filters:
+You need to add a filter for each column that needs filtering. For each filter, it should be populate as follows:
+- "equalsFilter": If we need to filter by only one value, then
+-- "column" should be the column name for which the filter is applied
+-- "type" should be "equalsFilter"
+-- "values" should contain only one value
+- "inFilter": If we need to filter by multiple values, then
+-- "column" should be the column name for which the filter is applied
+-- "FilterType' should be "inFilter"
+-- 'values' should contain multiple values
+- notInFilter: If we need to filter out by multiple values, then
+-- "column" should be the column name for which the filter is applied
+-- 'FilterType' should be "notInFilter"
+-- "values" should contain multiple values
+"""
+
+    # Crafting a question
+    system_setup = f"""given the following JSON schema:
+    
+    {json_schema}
+    
+    ---------
+    
+    return only the JSON data to describe the user question in terms of the JSON schema"""
+
+    data_structure = """
+    and given this data structure:
+    
+    dimension columns:
+    athlete,age,country,year,sport
+    
+    metric columns:
+    gold,silver,bronze,total
+    """
+
+    # loads from os.environ.get("OPENAI_API_KEY") - https://platform.openai.com/docs/quickstart/step-2-setup-your-api-key
+    client = OpenAI()
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": system_setup,
+            },
+            {
+                "role": "system",
+                "content": data_structure,
+            },
+            {
+                "role": "user",
+                "content": user_question,
+            }
+        ],
+        model="gpt-4-1106-preview",
+        max_tokens=4000,
+        response_format={"type": "json_object"}
+    )
+
+    # Extracting and printing the answer
+    response = chat_completion.choices[0].message.content.strip()
+
+    # Parse the JSON string to a Python dictionary
+    data = json.loads(response)
+
+    # Deserialize the dictionary to a Query instance
+    query = Query2.parse_obj(data)
+
+    df = get_dataframe(spark)
+    working_dataframe = df
+    final_dataframe = df
+
+    sum_aggregates = list(map(lambda column: "sum(" + column + ")", query.aggregateColumns))
+
+    if query.filters:
+        for filter in query.filters:
+            if filter.type == 'equalsFilter':
+                working_dataframe = working_dataframe.filter(filter.column + "==" + "'" + filter.values[0] + "'")
+            elif filter.type == 'inFilter':
+                working_dataframe = working_dataframe.filter(filter.column + " in (" + ",".join(map(lambda value: "'" + value + "'", filter.values)) + ")")
+            elif filter.type == 'notInFilter':
+                working_dataframe = working_dataframe.filter(filter.column + " not in (" + ",".join(map(lambda value: "'" + value + "'", filter.values)) + ")")
+
+    if query.drilledColumns:
+        working_dataframe = working_dataframe.groupby(*query.drilledColumns).sum(*query.aggregateColumns).orderBy(*sum_aggregates)
+
+    original_count = working_dataframe.count()
+
+    if query.top > 0 and query.bottom < original_count:
+        working_dataframe = working_dataframe.withColumn("index", monotonically_increasing_id())
+
+        top_dataframe = working_dataframe.limit(query.top)
+
+        bottom_dataframe = working_dataframe.orderBy(desc('index')).limit(query.bottom).orderBy(*sum_aggregates)
+
+        middle_dataframe = working_dataframe.subtract(top_dataframe).subtract(bottom_dataframe)
+
+        middle_dataframe = middle_dataframe.drop(*query.drilledColumns)
+
+        for drilledColumn in query.drilledColumns:
+            middle_dataframe = middle_dataframe.withColumn(drilledColumn, lit("middle"))
+
+        order_by_sum_aggregates = list(map(lambda column: "sum(sum(" + column + "))", query.aggregateColumns))
+        middle_dataframe = middle_dataframe.groupby(*query.drilledColumns).sum(*sum_aggregates).orderBy(*order_by_sum_aggregates)
+
+        top_dataframe = top_dataframe.drop("index")
+        bottom_dataframe = bottom_dataframe.drop("index")
+        final_dataframe = top_dataframe.union(middle_dataframe).union(bottom_dataframe)
+    else:
+        final_dataframe = working_dataframe
+
+    spark_data = final_dataframe.toJSON().collect()
+
+    return {
+        "data": spark_data,
+        "columns": final_dataframe.columns,
+        "allColumns": df.columns,
+        "originalCount": original_count
+    }
+
+
 @app.post("/query")
 async def data(query: Query, spark: SparkSession = Depends(get_spark_session)):
     df = get_dataframe(spark)
     working_dataframe = df
     final_dataframe = df
+
+    sum_aggregates = list(map(lambda column: "sum(" + column + ")", query.aggregateColumns))
 
     for filter in query.filters:
         if filter.type == 'equalsFilter':
@@ -88,7 +289,7 @@ async def data(query: Query, spark: SparkSession = Depends(get_spark_session)):
             working_dataframe = working_dataframe.filter(filter.column + " not in (" + ",".join(map(lambda value: "'" + value + "'", filter.values)) + ")")
 
     if query.drilledColumn:
-        working_dataframe = working_dataframe.groupby(query.drilledColumn).sum('gold', 'silver', 'bronze', 'total').orderBy('sum(gold)', 'sum(silver)', 'sum(bronze)', 'sum(total)')
+        working_dataframe = working_dataframe.groupby(query.drilledColumn).sum(*query.aggregateColumns).orderBy(*sum_aggregates)
 
     original_count = working_dataframe.count()
 
@@ -97,13 +298,16 @@ async def data(query: Query, spark: SparkSession = Depends(get_spark_session)):
 
         top_dataframe = working_dataframe.limit(query.top)
 
-        bottom_dataframe = working_dataframe.orderBy(desc('index')).limit(query.bottom).orderBy('sum(gold)', 'sum(silver)', 'sum(bronze)', 'sum(total)')
+        bottom_dataframe = working_dataframe.orderBy(desc('index')).limit(query.bottom).orderBy(*sum_aggregates)
 
         middle_dataframe = working_dataframe.subtract(top_dataframe).subtract(bottom_dataframe)
 
         middle_dataframe = middle_dataframe.drop(query.drilledColumn)
         middle_dataframe = middle_dataframe.withColumn(query.drilledColumn, lit("middle"))
-        middle_dataframe = middle_dataframe.groupby(query.drilledColumn).sum('sum(gold)', 'sum(silver)', 'sum(bronze)', 'sum(total)').orderBy('sum(sum(gold))', 'sum(sum(silver))', 'sum(sum(bronze))', 'sum(sum(total))')
+
+        order_by_sum_aggregates = list(map(lambda column: "sum(sum(" + column + "))", query.aggregateColumns))
+
+        middle_dataframe = middle_dataframe.groupby(query.drilledColumn).sum(*sum_aggregates).orderBy(*order_by_sum_aggregates)
 
         top_dataframe = top_dataframe.drop("index")
         bottom_dataframe = bottom_dataframe.drop("index")
